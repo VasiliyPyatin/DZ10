4e79277a6a31a788ce56efe2689db2325959b490
«Vasiliy» <pyatin_vs@mail.ru>
2022-07-27 22:05:10 +0300

    Задача 61

---
+++ task002/Program.cs
@@ -0,0 +1,71 @@
+﻿// Задача 61: Показать треугольник Паскаля. *Сделать вывод в виде равнобедренного треугольника.
+
+using System;
+using static System.Console;
+
+Clear();
+
+Write("Введите количество строк: ");
+
+int n = Convert.ToInt32(ReadLine());
+
+double[,] pascalTriangle = new double[n + 1, 2 * n + 1];
+
+FillPascalTriangle(pascalTriangle);
+
+WriteLine();
+PrintArray(pascalTriangle);
+
+TransformationPascalTriangle(pascalTriangle);
+
+Console.WriteLine();
+PrintArray(pascalTriangle);
+
+void TransformationPascalTriangle(double[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    int count = 0;
+    for (int j = array.GetLength(1) - 1; j >= 0; j--)
+    {
+      if (array[i, j] != 0)
+      {
+        array[i, array.GetLength(1) / 2 + j - count] = array[i, j];
+        array[i, j] = 0;
+        count++;
+      }
+    }
+  }
+  array[array.GetLength(0) - 1, 0] = 1;
+}
+
+void FillPascalTriangle(double[,] pascalTriangle)
+{
+  for (int k = 0; k < pascalTriangle.GetLength(0); k++)
+  {
+    pascalTriangle[k, 0] = 1;
+  }
+  for (int i = 1; i < pascalTriangle.GetLength(0); i++)
+  {
+    for (int j = 1; j < i + 1; j++)
+    {
+      pascalTriangle[i, j] = pascalTriangle[i - 1, j] + pascalTriangle[i - 1, j - 1];
+    }
+  }
+}
+
+void PrintArray(double[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      if (array[i, j] != 0)
+      {
+          Console.Write($"{array[i, j]} ");
+      }
+      else Console.Write("  ");
+    }
+    Console.WriteLine();
+  }
+}
